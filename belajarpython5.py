54a1a3b9b1d2ea39a95ac4ef79b454e2012eae98
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-07-18 09:11:05 +0700

    Create belajarpython5.py

---
+++ belajarpython5.py
@@ -0,0 +1,18 @@
+# turotial input data user
+
+# data yang dimasukan pasti string
+data = input ("masukan nama: ")
+
+print ("data =",data, "type =",type (data))
+
+# jika kita mau mengambil integer,maka
+angka = float (input ("masukan angka :"))
+angka = int (input ("masukan angka :"))
+
+print ("data =",angka, "type =",type (angka))
+
+# bagaimana dengan boolean??
+
+biner = bool(int(input("masukan nilai boolean: ")))
+
+print ("data =",biner, "type =",type (biner))
