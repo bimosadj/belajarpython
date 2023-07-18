bfaab41dd7814afabfb1fd4dfe5d99036949908c
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-07-18 09:01:38 +0700

    Create belajarpython1.py

---
+++ belajarpython1.py
@@ -0,0 +1,23 @@
+# Latihan 1 start
+
+import time
+
+start_time = time.time()
+
+print("hello")
+print("hello")
+print("hello")
+print("hello world")
+
+print("halo kawan")
+a = 10  # ini komen juga cuman beda rasa
+# ini adalah komentar
+"""sama aja cuman beda rasa"""
+print(a)
+for i in range(1, 1000):
+    a = 10
+
+print(time.time() - start_time, "detik")
+
+# kita bisa mengcompile python ke
+# yang namanya byte code
