535b30f2b192243ace1fa92c9ba2a7e145e4a095
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-07-18 09:29:18 +0700

    Update belajarpython7.py
    menambahkan tugas dari yt

--- belajarpython7.py
+++ belajarpython7.py
@@ -18,3 +18,26 @@ print ("suhu dalam fahrenheit adalah ",fahrenheit,"fahrenheit")
 # kelvin
 kelvin = celcius + 273
 print ("suhu dalam kelvin adalah ",kelvin,"kelvin")
+
+
+# pr
+# tentukan rumus konversi fahrenheit ke kelvin
+print ("\n PROGRAM KONVERSI TEMPERATUR\n")
+
+fahrenheit = float(input('masukan suhu dalam fahrenheit : '))
+print ("suhu adalah ",fahrenheit,"fahrenheit")
+
+celcius = ((5/9)*fahrenheit)- 32 
+kelvin = celcius + 273
+print ("suhu dalam kelvin adalah ",kelvin,"kelvin")
+
+# tentukan rumus konversi kelvin ke fahrenheit
+
+print ("\n PROGRAM KONVERSI TEMPERATUR\n")
+
+kelvin = float(input('masukan suhu dalam kelvin : '))
+print ("suhu adalah ",kelvin,"kelvin")
+
+celcius = kelvin - 273
+kelvin = ((5/9)*fahrenheit)- 32 
+print ("suhu dalam fahrenheit adalah ",fahrenheit,"fahrenheit")
