62fea02876d28a6dcdae50867c648fba44d98df5
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-07-18 09:03:11 +0700

    Create belajarpython3.py

---
+++ belajarpython3.py
@@ -0,0 +1,41 @@
+# a - 10,a adalah variabel dengan nilai 10
+
+# tipe data: Angka satuan (integer)
+from traceback import print_tb
+
+
+data_integer = 1
+print ("data :", data_integer)
+print ("_ bertipe :", type(data_integer))
+
+# tipe data :angka dengan koma (float)
+data_float = 1.5
+print("data : ", data_float)
+print ("_betipe :", type(data_integer))
+
+# tipe data: kumpulan karakter (string)
+data_string = "jamal"
+print("data : ", data_float)
+print ("_betipe :", type(data_integer))
+
+# tipe data: biner true/false (boolean)
+data_bool = "false"
+print("data : ", data_bool)
+print ("_betipe :", type(data_bool))
+
+# tipe data khusus
+
+
+# bilangan complex
+data_complex = complex (5,6)
+data_complex = "data_complex"
+print ("_betipe :", type(data_complex))
+
+# tipe data dari bahasa c
+
+from ctypes import c_double
+
+data_c_double = c_double (10.5)
+print ("data :,data_c_double")
+print ("_betipe :", type(data_c_double))
+
