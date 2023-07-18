d3a91d18dc4eb5ffc928161f97d6eccbfcfccd49
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-07-18 09:13:16 +0700

    Create belajarpython6.py

---
+++ belajarpython6.py
@@ -0,0 +1,43 @@
+# operasi aritmatika
+
+a = 10
+b = 3
+
+# operasi tambah +
+hasil = a+b
+print (a,'+',b,'=',hasil)
+
+# operasi kurang -
+hasil = a-b
+print (a,'-',b,'=',hasil)
+
+# operasi kali x
+hasil = a*b
+print (a,'*',b,'=',hasil)
+
+# operasi pembagian /
+hasil = a/b
+print (a,'/',b,'=',hasil)
+
+# operasi eksponen (pangkat) **
+hasil = a ** b
+print (a,'**',b,'=',hasil)
+
+# operasi modulus %
+hasil = a % b
+print (a,'%',b,'=',hasil)
+
+# operasi flor division //
+hasil = a // b
+print (a,'//',b,'=',hasil)
+
+# prioritas operasi, operasional precedence
+
+x = 3
+y = 2
+z = 4
+hasil  = x ** y * z + x / y - y % z // x
+
+print (x,'**',y, '*',z,'+',x,'/',y,'-',y,'%',z,'//',x,'=',hasil)
+
+# print (x,'**',y,'*',z,'+',x,'/',y,'-'y,'%',z,'//',x,'=',hasil)
