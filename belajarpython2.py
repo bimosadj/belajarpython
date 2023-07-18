ddf3d7c9ec29c0d24c1e6ca50de5405b9b97315d
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-07-18 09:02:28 +0700

    Create belajarpython2.py

---
+++ belajarpython2.py
@@ -0,0 +1,31 @@
+# variabel=yang sama di matematika
+# contoh x=2 y=x+4 = 2+4 =6
+# x=variabel 2=value (=) asegment
+
+"""ini belajar pengenalan variabel"""
+
+
+# variabel adalah tempat menyimpan
+# menaruh / assigment nilai
+a = 10
+x = 5
+panjang = 1000
+
+# pemanggilan 1
+print("Nilai a =", a)
+print("Nilai x =", x)
+print("Nilai panjang =", panjang)
+
+# penamaan
+nilai_y = 15  # underscore boleh
+juta10 = 10000000  # ini boleh
+nilaiZ = 17.5  # ini boleh juga
+
+# pemanggilan 2
+print("Nilai a =", a)
+a = 7
+print("Nilai a =", a)
+
+# assigment indirect
+b = a
+print("Nilai b =", b)
