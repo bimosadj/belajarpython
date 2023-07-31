597acf05d27767fedde046440201cb9d1a9e4396
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-07-31 15:35:25 +0700

    Create python16.py

---
+++ python16.py
@@ -0,0 +1,10 @@
+# date and time (latihan)
+
+import datetime as dt
+
+print("Silahkan masukan tanggal, \nbulan, dan tahun lahir anda\n")
+tanggal = input("Tanggal \t:")
+bulan = input ("bulan\t:")
+tahun = input ("tahun\t:")
+
+
