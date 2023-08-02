50ebc86db6386ee11ff6d4f77467e0cdc0251b4b
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-08-02 10:45:36 +0700

    Update python16.py
    penambahan beberapa function

--- python16.py
+++ python16.py
@@ -3,8 +3,18 @@
 import datetime as dt
 
 print("Silahkan masukan tanggal, \nbulan, dan tahun lahir anda\n")
-tanggal = input("Tanggal \t:")
-bulan = input ("bulan\t:")
-tahun = input ("tahun\t:")
+tanggal =int(input("Tanggal \t:"))
+bulan = int(input ("Bulan\t\t:"))
+tahun = int (input ("Tahun\t\t:"))
 
+tanggal_lahir = dt.date(tahun,bulan,tanggal)
+print(f"tanggal lahir anda adalah :{tanggal_lahir}")
 
+hari_ini = dt.date.today()
+print(f"hari ini tanggal ; {hari_ini}")
+umur_hari = hari_ini - tanggal_lahir
+umur_tahun = umur_hari.days / 365
+umur_bulan_sisa = (umur_hari % 365) //30 
+print(f"Hari nya adalah :{tanggal_lahir:%A}")
+print(umur_hari.days)
+print(f"umur anda adalah {umur_tahun}tahun")
