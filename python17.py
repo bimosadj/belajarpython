df7daaa7451df6f7e5c2b95c895bb44d3f11b7cc
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-08-02 14:08:41 +0700

    Create python17.py

---
+++ python17.py
@@ -0,0 +1,30 @@
+# If dan else Statment
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksi nya
+
+nama = input ("Siapa nama anda? ")
+
+##programs sebelumnya
+# if kondisi : aksi
+# program selanjutnya
+
+# 1.program if inline
+if nama=="ucup" :  print ("lu asik banget!!!")
+print(f"Terima kasih {nama}")
+
+# 2.program if indentation
+
+if nama=="ucup":
+        print("Lu asik banget bang ! ")
+        print("Lu juga asik bang !")
+print (f"Terima kasih {nama}")
+
+# 3. Else statment
+
+if nama=="otong":
+        print ("hai otong,si paling keren")
+else:
+        print("Ah lu bukan otong,lu enggak keren! :(")
+print("akhir dari program")
