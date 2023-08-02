fec6833aaa02a93f673f81cffc73bb4b68ba7e91
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-08-02 11:04:05 +0700

    Update python16.py
    menambahkan beberapa fitur

--- python16.py
+++ python16.py
@@ -18,3 +18,25 @@ umur_bulan_sisa = (umur_hari % 365) //30
 print(f"Hari nya adalah :{tanggal_lahir:%A}")
 print(umur_hari.days)
 print(f"umur anda adalah {umur_tahun}tahun")
+
+# new file
+# date and time (latihan)
+
+import datetime as dt
+
+print("Silahkan masukan tanggal, \nbulan, dan tahun lahir anda\n")
+tanggal =int(input("Tanggal \t:"))
+bulan = int(input ("Bulan\t\t:"))
+tahun = int (input ("Tahun\t\t:"))
+
+tanggal_lahir = dt.date(tahun,bulan,tanggal)
+print(f"tanggal lahir anda adalah :{tanggal_lahir}")
+
+hari_ini = dt.date.today()
+print(f"hari ini tanggal ; {hari_ini}")
+umur_hari = hari_ini - tanggal_lahir
+umur_tahun = umur_hari.days / 365
+umur_bulan_sisa = (umur_hari.days % 365) //30 
+
+print(f"Hari nya adalah :{tanggal_lahir:%A}")
+print(f"umur anda adalah {umur_tahun}tahun, {umur_bulan_sisa}bulan sisa")
