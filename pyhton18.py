513e1d39942fafe793dd546fa8e17082daf2a2f3
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-08-02 14:57:24 +0700

    Create pyhton18.py

---
+++ pyhton18.py
@@ -0,0 +1,14 @@
+# ELIF = else dan if statment
+
+nama = input("Nama kamu siapa? ")
+
+if nama== "ucup": #kondisi 1
+    print("Hai mass bro !") #aksi true 1
+elif nama == "otong": #kondisi 2
+    print("Hai si paling kece!!") #aksi true 2
+elif nama == "mario": #kondisi 3
+    print ("hari pelawak!!!") #aksi true 3
+else:
+    print("au ahh gelap enggak kenal!!!")
+
+print("ini adalah akhir dari program")
