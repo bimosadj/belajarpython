496ba4319949aaf8dc4a6d30775c6c08964ef906
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-08-04 10:06:04 +0700

    Create python20.py

---
+++ python20.py
@@ -0,0 +1,38 @@
+# perulangan (loop)
+
+# for kondisi :
+#   aksi
+
+# ini dengan list
+angka2_list = [0,2,4,8,10] # ini adalah list
+print(angka2_list)
+
+for i in angka2_list:
+    print(f"i sekarang -> {i}")
+
+print("akhir program 1 \n")
+
+# ini dengan range
+angka2_range = range(5)
+
+for i in angka2_range:
+    print(f"i sekarang -> {i}")
+
+print("akhir dari program 2\n")
+
+# ini dengan range
+angka2_range = range(1,10)
+
+for i in angka2_range:
+    print(f"i sekarang -> {i}")
+
+print("akhir dari program 3 \n")
+
+# menggunakan string
+
+data_str = "gwej kece abiessss"
+
+for huruf in data_str:
+    print(huruf)
+
+print("akhir dari program 4\n")
