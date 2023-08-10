2f0f7a36787509cafbcb461b96f104ca1a063ef4
bimosadj <139105556+bimosadj@users.noreply.github.com>
2023-08-10 10:07:44 +0700

    Update python24.py
    mengubah kode pada point nomer 3,dan juga menambahkan beberapa kode

--- python24.py
+++ python24.py
@@ -1,48 +1,64 @@
 # latihan perulangan membuat segitiga
 
-sisi = 10
+sisi = 9
 
 # 1.Menggunakan for
-
 # dummy variabel
-
 print("awal dari for")
 count = 1
 for i in range(sisi):
     print ("*"*count)
-    count += 1
-    
-print ("akhir dari for\n")
-# 2.Menggunakan while
+    count +=1
 
+print("akhir dari for")
+# 2.menggunaka while
 print("awal dari while")
 count = 1
 while True:
-    print ("*"*count)
+    print("*"*count)
     count +=1
 
     if count > sisi:
         break
-print("akhir dari while\n")
 
-# 3.Hanya ganjil saja
-
+print("akhir dari while")
+# 3.hanya ganjil saja
 print("awal dari while")
 count = 1
 while True:
-    if(count%2):
-    #print jika ganjil
-    print("*"*count)
-    count += 1
-    
+    if (count%2):
+        # print jika ganjil
+        print("*"*count)
+        count +=1
+
     else:
-    # akan kembali keatas jika ganjil
-        count += 1
+        # akan kembali ke atas jika ganjil 
+        count +=1
         continue 
-    
-    # Akan break jika count melebihi sisi   
+
+    # akan break jika count melebihi sisi
     if count > sisi:
         break
+print("akhir dari while")
 
+# 4.hanya ganjil saja
+print("awal dari while")
+count = 1
+spasi = int(sisi/2)
+
+while True:
+    if (count%2):
+        # print jika ganjil
+        print(" "*spasi , "+"*count)
+        spasi -=1
+        count +=1
+
+    else:
+        # akan kembali ke atas jika ganjil 
+        count +=1
+        continue 
+
+    # akan break jika count melebihi sisi
+    if count > sisi:
+        break
 print("akhir dari while\n")
-
